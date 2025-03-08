a024cc06a0085b6040376088d35b280c9b7c7c70
kogantidezerae <kogantidezerae@gmail.com>
2025-03-08 12:56:10 +0000

    Add 3.c

---
+++ src/3.c
@@ -0,0 +1,10 @@
+int main() {
+    int a = 5;
+    int b = 3;
+    int result = add(a, b);
+    printf("The sum of %d and %d is %d\n", a, b, result);
+}
+
+int add(int x, int y) {
+    return x + y;
+}
