1457b6e6d70c3b0764704165d5d0e23e9bfa8e35
kogantidezerae <kogantidezerae@gmail.com>
2025-04-26 03:51:17 +0000

    Add 32.c

---
+++ src/32.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+    int num1 = 5;
+    int num2 = 3;
+
+    printf("Addition: %d + %d = %d\n", num1, num2, (num1 + num2));
+
+    printf("Subtraction: %d - %d = %d\n", num1, num2, (num1 - num2));
+
+    printf("Multiplication: %d * %d = %d\n", num1, num2, (num1 * num2));
+
+    printf("Division: %d / %d = %d\n", num1, num2, (num1 / num2));
+}
