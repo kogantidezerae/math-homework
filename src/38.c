6caefff1f4b4ce7dd49e3a719e2cc50a4cc6583d
kogantidezerae <kogantidezerae@gmail.com>
2025-05-19 17:18:38 +0000

    Add 38.c

---
+++ src/38.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 20;
+
+    printf("x + y = %d\n", x + y);
+    printf("x - y = %d\n", x - y);
+    printf("x * y = %d\n", x * y);
+    printf("x / y = %f\n", (float)x / y);
+    
+    return 0;
+}
